f1a7fc12f83bc3638cb2806a8e158ce7ffbda9ff
xtile tetris <xtile@yandex.ru>
2021-10-17 22:27:10 +0000

    deflating binary data from okex

--- websocket.go
+++ websocket.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"compress/flate"
 	"log"
 	"os"
 	"os/signal"
@@ -13,12 +15,18 @@ func main() {
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
-	socket := gowebsocket.New("wss://stream.binance.com:9443/ws/BTCUSDT@kline_1m")
+	// binance
+	//socket := gowebsocket.New("wss://stream.binance.com:9443/ws/BTCUSDT@kline_1m")
+	// okex
+	socket := gowebsocket.New("wss://real.okex.com:8443/ws/v3")
 
 	socket.OnConnected = func(socket gowebsocket.Socket) {
 		log.Println("Connected to server")
+		//binance
 		//socket.SendText("{  \"method\": \"SUBSCRIBE\",  \"params\": [    \"btcusdt@kline_1m\" ],  \"id\": 1}")
-		socket.SendText("{  \"method\": \"SUBSCRIBE\",  \"params\": [    \"btcusdt@trade\" ],  \"id\": 1}")
+		//socket.SendText("{  \"method\": \"SUBSCRIBE\",  \"params\": [    \"btcusdt@trade\" ],  \"id\": 1}")
+		//okex
+		socket.SendText("{\"op\": \"subscribe\", \"args\": [\"spot/ticker:BTC-USDT\"]}")
 
 		/*
 			{  "method": "SUBSCRIBE",  "params": [    "btcusdt@aggTrade",    "btcusdt@depth"  ],  "id": 1}
@@ -35,6 +43,14 @@ func main() {
 
 	socket.OnBinaryMessage = func(data []byte, socket gowebsocket.Socket) {
 		log.Println("Recieved binary data ", data)
+		// deflating compressed binary data from OKex
+		var b bytes.Buffer
+		r := flate.NewReader(bytes.NewReader(data))
+		b.ReadFrom(r)
+		r.Close()
+		strData := string(b.Bytes())
+		log.Println("inflated data ", strData)
+
 	}
 
 	socket.OnPingReceived = func(data string, socket gowebsocket.Socket) {
