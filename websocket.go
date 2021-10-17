ca16c3ef0aba19e3f50f86fb3678d556065b046b
xtile tetris <xtile@yandex.ru>
2021-10-17 23:05:58 +0000

    getting data from binance, huobi, okex

--- websocket.go
+++ websocket.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bytes"
 	"compress/flate"
+	"compress/gzip"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/signal"
@@ -12,13 +14,24 @@ import (
 
 func main() {
 
+	exchange := "HUOBI"
+	//exchange = "OKEX"
+	//exchange = "BINANCE"
+
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
+	socket := gowebsocket.New("")
+	if exchange == "BINANCE" {
+		// binance
+		socket = gowebsocket.New("wss://stream.binance.com:9443/ws/BTCUSDT@kline_1m")
+	} else if exchange == "HUOBI" {
+		// huobi
+		socket = gowebsocket.New("wss://api.huobi.pro/ws")
 
-	// binance
-	//socket := gowebsocket.New("wss://stream.binance.com:9443/ws/BTCUSDT@kline_1m")
-	// okex
-	socket := gowebsocket.New("wss://real.okex.com:8443/ws/v3")
+	} else if exchange == "OKEX" {
+		// okex
+		socket = gowebsocket.New("wss://real.okex.com:8443/ws/v3")
+	}
 
 	socket.OnConnected = func(socket gowebsocket.Socket) {
 		log.Println("Connected to server")
@@ -26,7 +39,10 @@ func main() {
 		//socket.SendText("{  \"method\": \"SUBSCRIBE\",  \"params\": [    \"btcusdt@kline_1m\" ],  \"id\": 1}")
 		//socket.SendText("{  \"method\": \"SUBSCRIBE\",  \"params\": [    \"btcusdt@trade\" ],  \"id\": 1}")
 		//okex
-		socket.SendText("{\"op\": \"subscribe\", \"args\": [\"spot/ticker:BTC-USDT\"]}")
+		//socket.SendText("{\"op\": \"subscribe\", \"args\": [\"spot/ticker:BTC-USDT\"]}")
+
+		//huobi
+		socket.SendText("{ \"sub\": \"market.btcusdc.ticker\", \"id\": \"1\" }")
 
 		/*
 			{  "method": "SUBSCRIBE",  "params": [    "btcusdt@aggTrade",    "btcusdt@depth"  ],  "id": 1}
@@ -43,13 +59,40 @@ func main() {
 
 	socket.OnBinaryMessage = func(data []byte, socket gowebsocket.Socket) {
 		log.Println("Recieved binary data ", data)
-		// deflating compressed binary data from OKex
-		var b bytes.Buffer
-		r := flate.NewReader(bytes.NewReader(data))
-		b.ReadFrom(r)
-		r.Close()
-		strData := string(b.Bytes())
-		log.Println("inflated data ", strData)
+
+		if exchange == "BINANCE" {
+			// binance
+			socket = gowebsocket.New("wss://stream.binance.com:9443/ws/BTCUSDT@kline_1m")
+		} else if exchange == "HUOBI" {
+			// huobi
+			strDataOriginal := string(data)
+			log.Println("original data ", strDataOriginal)
+
+			bz := bytes.NewReader(data[:])
+			z, err := gzip.NewReader(bz)
+			if err != nil {
+				log.Println("error1 ", err)
+				return
+			}
+			defer z.Close()
+			p, err := ioutil.ReadAll(z)
+			if err != nil {
+				log.Println("error2 ", err)
+				return
+			}
+			strDataGzip := string(p)
+			log.Println("decoded message:  ", strDataGzip)
+
+		} else if exchange == "OKEX" {
+			// okex
+			// deflating compressed binary data from OKex
+			var b bytes.Buffer
+			r := flate.NewReader(bytes.NewReader(data))
+			b.ReadFrom(r)
+			r.Close()
+			strData := string(b.Bytes())
+			log.Println("inflated data ", strData)
+		}
 
 	}
 
